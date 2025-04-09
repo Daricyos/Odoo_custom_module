9f85a7a67d779a6d286db2e3d650943cb7fe61a4
daricyos <daricyos@gmail.com>
2025-04-09 15:49:12 +0300

    fix bug product_uom_qty

--- stock_attachment/models/stock_move.py
+++ stock_attachment/models/stock_move.py
@@ -1,10 +1,11 @@
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class StockMoveInherit(models.Model):
     _inherit = 'stock.move'
 
-    actual_costs = fields.Float('Фактичні витрати', digits=(16, 3), required=True)
+    actual_costs = fields.Float('Фактичні витрати', digits=(16, 3), required=True, default=None)
     actual_yield_factor = fields.Float('Фактичний коефіцієнт виходу', compute="_compute_actual_yield_factor", store=True)
 
     @api.depends('raw_material_production_id.product_qty', 'actual_costs')
@@ -14,3 +15,14 @@ class StockMoveInherit(models.Model):
                 record.actual_yield_factor = (record.raw_material_production_id.product_qty / record.actual_costs) * 100
             else:
                 record.actual_yield_factor = 0
+
+    @api.constrains('actual_costs')
+    def _check_actual_costs(self):
+        for record in self:
+            if not record.actual_costs or record.actual_costs <= 0:
+                raise ValidationError("Поле 'Фактичні витрати' є обов'язковим і має бути більше нуля.")
+
+    @api.onchange('actual_costs')
+    def _onchange_actual_costs(self):
+        if self.actual_costs:
+            self.product_uom_qty = self.actual_costs
