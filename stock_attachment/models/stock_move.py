3c93daf03c0f7c420eb7d0e89e17b4fa405f10ce
LobodaRomanEvgenovich <loboda.rom@gmail.com>
2025-12-02 12:33:04 +0200

    tasks - 1976

--- stock_attachment/models/stock_move.py
+++ stock_attachment/models/stock_move.py
@@ -4,14 +4,20 @@ from odoo import api, fields, models
 class StockMoveInherit(models.Model):
     _inherit = 'stock.move'
 
-    actual_costs = fields.Float('Фактичні витрати', digits=(16, 3), required=True, default=None)
+    actual_costs = fields.Float(
+        'Фактичні витрати',
+        digits=(16, 3),
+        # required=True,
+        # default=None
+        )
     actual_yield_factor = fields.Float('Фактичний коефіцієнт виходу', compute="_compute_actual_yield_factor", store=True)
 
     @api.depends('raw_material_production_id.product_qty', 'actual_costs')
     def _compute_actual_yield_factor(self):
         for record in self:
             if record.raw_material_production_id.product_qty > 0 and record.actual_costs != 0:
-                record.actual_yield_factor = (record.raw_material_production_id.product_qty / record.actual_costs) * 100
+                # record.actual_yield_factor = (record.raw_material_production_id.product_qty / record.actual_costs) * 100
+                record.actual_yield_factor = record.raw_material_production_id.product_qty / record.actual_costs
             else:
                 record.actual_yield_factor = 0
 
